114f75aa01efad360fa1f3a2d9eaabe7dad76057
Nongcebo <nongcebot@gmail.com>
2023-03-20 03:15:39 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the lowercase
+ * alphabet in reverse, followed by a new line.
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+char i;
+
+for (i = 'z' ; i >= 'a' ; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
